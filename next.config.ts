3ab8a7ce81f140600e06408eff003f9715c26a58
Zafer <zaferabdo85@gmail.comr>
2025-12-02 09:26:34 +0200

    added header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gist.github.com",
+        pathname: "/user-attachments/assets/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
